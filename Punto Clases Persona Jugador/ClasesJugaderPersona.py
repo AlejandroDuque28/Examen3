17a4e48f842088b1bdeab8e39d70368ed26e1a9f
AlejandroDuque28 <aduque94656@umanizales.edu.com>
2022-05-24 21:14:36 -0500

    Se sube el punto de las clases de persona jugador

---
+++ Punto Clases Persona Jugador/ClasesJugaderPersona.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+
+class Persona:
+    nombre : str
+    nif : str
+    fechaNac : date
+#========================================
+
+class Jugador(Persona):
+    numFed: int
